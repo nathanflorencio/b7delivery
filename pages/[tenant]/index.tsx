77f622a9d9222f2bc1492ec40466080f93c6ecf4
Nathan Florencio <nflorencio@gmail.com>
2022-08-02 13:45:39 -0300

    Menu Screen - Home

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -1,7 +1,36 @@
+import { SearchInput } from '../../components/SearchInput';
+import styles from '../../styles/Home.module.css';
+
 const Home = () => {
+
+  const handleSearch = (searchValue: string) => {
+    console.log(`Você está buscando por: ${searchValue}`);
+  }
+
   return (
-    <div>
-      ...
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.headerTop}>
+          <div className={styles.headerTopLeft}>
+            <div className={styles.headerTitle}>Seja Bem Vindo (a) 👋</div>
+            <div className={styles.headerSubtitle}>O que deseja para hoje?</div>
+          </div>
+          <div className={styles.headerTopRight}>
+            <div className={styles.menuButton}>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+              <div className={styles.menuButtonLine}></div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.headerBottom}>
+          <SearchInput
+            mainColor="#FB9400"
+            onSearch={handleSearch}
+
+          />
+        </div>
+      </header>
     </div>
   );
 }
