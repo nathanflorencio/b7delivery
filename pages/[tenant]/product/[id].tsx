1a92faa7ed43bb49dfcf8b56d583f61a897fd9bb
Nathan Florencio <nflorencio@gmail.com>
2022-08-04 14:28:54 -0300

    Created product screen.

---
+++ pages/[tenant]/product/[id].tsx
@@ -0,0 +1,100 @@
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import { useEffect } from 'react';
+import { Button } from '../../../components/Button';
+import { Header } from '../../../components/Header';
+import { useAppContext } from '../../../contexts/AppContext';
+import { useApi } from '../../../libs/useApi';
+import { useFormatter } from '../../../libs/useFormatter';
+import styles from '../../../styles/Product-id.module.css';
+import { Product } from '../../../types/Product';
+import { Tenant } from '../../../types/Tenants';
+
+const Product = (data: Props) => {
+  const { tenant, setTenant } = useAppContext();
+
+  useEffect(() => {
+    setTenant(data.tenant);
+  }, []);
+
+  const formatter = useFormatter();
+  const handleAddToCart = () => {
+
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{data.product.name} | {data.tenant.name}</title>
+      </Head>
+
+      <div className={styles.headerArea}>
+        <Header
+          color={data.tenant.mainColor}
+          backHref={`/${data.tenant.slug}`}
+          title="Produto"
+          invert
+        />
+      </div>
+
+      <div className={styles.headerBg} style={{ backgroundColor: data.tenant.mainColor }}></div>
+
+      <div className={styles.productImage}>
+        <img src={data.product.image} alt="" />
+      </div>
+
+      <div className={styles.category}>{data.product.categoryName}</div>
+      <div className={styles.title} style={{ borderBottomColor: data.tenant.mainColor }}>{data.product.name}</div>
+      <div className={styles.line}></div>
+
+      <div className={styles.description}>{data.product.description}</div>
+
+      <div className={styles.qtText}>Quantidade</div>
+      <div className={styles.area}>
+        <div className={styles.areaLeft}>...</div>
+        <div
+          className={styles.areaRight}
+          style={{ color: data.tenant.mainColor }}
+        >{formatter.formatPrice(data.product.price)}</div>
+      </div>
+
+      <div className={styles.buttonArea}>
+        <Button
+          color={data.tenant.mainColor}
+          label="Adicionar a sacola"
+          onClick={handleAddToCart}
+          fill
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Product;
+
+type Props = {
+  tenant: Tenant,
+  product: Product
+}
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { tenant: tenantSlug, id } = context.query;
+  const api = useApi(tenantSlug as string);
+
+  // Get Tenant
+  const tenant = await api.getTenant();
+  if (!tenant) {
+    return {
+      redirect: { destination: '/', permanent: false }
+    }
+  }
+
+  // Get Products
+  const product = await api.getProduct(id as string);
+
+  return {
+    props: {
+      tenant,
+      product
+    }
+  }
+}
